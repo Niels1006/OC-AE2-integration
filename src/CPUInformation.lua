1ef77387b686a722a394302f3b569cd3c5a09d9f
somepersondontask <geomertydash10@gmail.com>
2024-04-03 17:17:31 -0400

    started: can print full cpu status

--- src/CPUInformation.lua
+++ src/CPUInformation.lua
@@ -1,1 +1,14 @@
+local component = require("component")
+local ME = component.me_interface
+local AE2CPUTable = ME.getCpus()
+
+function pprint(table)
+    for key, value in pairs(table) do
+        print(key, value)
+    end
+end
+
+for i=1,#AE2CPUTable do
+    pprint(AE2CPUTable[i])
+end
 
